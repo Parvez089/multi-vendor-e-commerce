3158239cb295ec44ac11bd97ed70498a66f57269
parvez <mhparvez089@gmail.com>
2025-05-28 20:13:56 +0600

    05: search filters

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,22 +1,52 @@
 
 
-
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
+import { Category } from "@payload-types";
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
+import { SearchFilters } from "./search-filters";
 
-interface Props{
-    children: React.ReactNode;
+interface Props {
+  children: React.ReactNode;
 }
 
-const Layout = ({children}: Props) =>{
-    return (
-      <div className='flex flex-col min-h-screen'>
-        <Navbar />
-        <div className="flex-1 bg-[#F4F4F0]">{children}</div>
+const Layout = async ({ children }: Props) => {
+  const payload = await getPayload({
+    config: configPromise,
+  });
 
-        <Footer />
-      </div>
-    );
-}
+  const data = await payload.find({
+    collection: "categories",
+    depth: 1, //Load subcategory
+    pagination: false,
+    where: {
+      parent: {
+        exists: false,
+      },
+    },
+  });
+
+  const formattedData = data.docs.map((doc) => ({
+    ...doc,
+    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+      ...(doc as Category),
+      subcategories: undefined,
+    })),
+  }));
+  console.log({
+    data,
+    formattedData,
+  });
+  return (
+    <div className='flex flex-col min-h-screen'>
+      <Navbar />
+      <SearchFilters data={formattedData} />
+      <div className='flex-1 bg-[#F4F4F0]'>{children}</div>
+
+      <Footer />
+    </div>
+  );
+};
 
 export default Layout;
