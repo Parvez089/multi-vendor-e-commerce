b28eda437d239ad51cf92d4c5554bc2b6863f702
parvez <mhparvez089@gmail.com>
2025-05-30 21:21:33 +0600

    06: categories finalization

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -6,6 +6,7 @@ import { Category } from "@payload-types";
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
 import { SearchFilters } from "./search-filters";
+import { CustomCategory } from "./types";
 
 interface Props {
   children: React.ReactNode;
@@ -25,19 +26,17 @@ const Layout = async ({ children }: Props) => {
         exists: false,
       },
     },
+    sort: "name",
   });
 
-  const formattedData = data.docs.map((doc) => ({
+  const formattedData: CustomCategory[] = data.docs.map((doc) => ({
     ...doc,
     subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
       ...(doc as Category),
       subcategories: undefined,
     })),
   }));
-  console.log({
-    data,
-    formattedData,
-  });
+
   return (
     <div className='flex flex-col min-h-screen'>
       <Navbar />
--- src/app/(app)/(home)/search-filters/subcategory-menu.tsx
+++ src/app/(app)/(home)/search-filters/subcategory-menu.tsx
@@ -1,51 +1,45 @@
 import { Category } from "@/payload-types";
 import Link from "next/link";
+import { CustomCategory } from "../types";
 
-interface Props{
-    category: Category;
-    isOpen: boolean;
-    position: {top: number; left: number};
+interface Props {
+  category: CustomCategory;
+  isOpen: boolean;
+  position: { top: number; left: number };
 }
 
+export const SubcategoryMenu = ({ category, isOpen, position }: Props) => {
+  if (
+    !isOpen ||
+    !category.subcategories ||
+    category.subcategories.length === 0
+  ) {
+    return null;
+  }
 
-export const SubcategoryMenu = ({
-    category,
-    isOpen,
-    position,
-}:Props)=>{
- 
-    if (
-      !isOpen ||
-      !category.subcategories ||
-      category.subcategories.length === 0
-    ) {
-      return null;
-    }
+  const backgroundColor = category.color || "#F5F5F5";
 
-    const backgroundColor = category.color || "#F5F5F5";
-
-
-    return (
+  return (
+    <div
+      className='fixed z-100'
+      style={{
+        top: position.top,
+        left: position.left,
+      }}>
+      {/* Invisible bridge to maintain hover */}
+      <div className='h-3 w-60' />
       <div
-        className='fixed z-100'
-        style={{
-          top: position.top,
-          left: position.left,
-        }}>
-        {/* Invisible bridge to maintain hover */}
-        <div className='h-3 w-60' />
-        <div
-          style={{ backgroundColor }}
-          className='w-60 text-black rounded-md overflow-hidden border shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[2px] -translate-y-[2px]'>
-          {category.subcategories?.map((subcategory: Category) => (
-            <Link
-              key={subcategory.slug}
-              href='/'
-              className='w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center underline font-medium'>
-              {subcategory.name}
-            </Link>
-          ))}
-        </div>
+        style={{ backgroundColor }}
+        className='w-60 text-black rounded-md overflow-hidden border shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[2px] -translate-y-[2px]'>
+        {category.subcategories?.map((subcategory: Category) => (
+          <Link
+            key={subcategory.slug}
+            href={`/${category.slug}/${subcategory.slug}`}
+            className='w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center underline font-medium'>
+            {subcategory.name}
+          </Link>
+        ))}
       </div>
-    );
-}
+    </div>
+  );
+};
