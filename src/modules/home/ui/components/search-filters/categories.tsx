be55a1ae21f97dd2158d7ee1176069cc164e98de
parvez <mhparvez089@gmail.com>
2025-06-17 20:45:09 +0600

    10: category pages

--- src/modules/home/ui/components/search-filters/categories.tsx
+++ src/modules/home/ui/components/search-filters/categories.tsx
@@ -1,19 +1,24 @@
 /** @format */
 "use client";
-import { CategoryDropdown } from "./category-dropdown";
-
-import { useEffect, useRef, useState } from "react";
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 import { ListFilterIcon } from "lucide-react";
-import { CategoriesSidebar } from "./categories-sidebar";
+import { useEffect, useRef, useState } from "react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
 import { CategoriesGetManyOutput } from "@/modules/categories/types";
+
+import { CategoryDropdown } from "./category-dropdown";
+import { CategoriesSidebar } from "./categories-sidebar";
+import { useParams } from "next/navigation";
 
 interface Props {
   data: CategoriesGetManyOutput;
 }
 
 export const Categories = ({ data }: Props) => {
+  const params = useParams();
+
   const containerRef = useRef<HTMLDivElement>(null);
   const measureRef = useRef<HTMLDivElement>(null);
   const viewAllRef = useRef<HTMLDivElement>(null);
@@ -22,7 +27,9 @@ export const Categories = ({ data }: Props) => {
   const [isAnyHovered, setIsAnyHovered] = useState(false);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  const activeCategory = "all";
+  const categoryParam = params.category as string | undefined;
+  const activeCategory = categoryParam || "all";
+
   const activeCategoryIndex = data.findIndex(
     (cat) => cat.slug === activeCategory
   );
@@ -93,6 +100,7 @@ export const Categories = ({ data }: Props) => {
         ))}
         <div ref={viewAllRef} className='shrink-0'>
           <Button
+            variant='elevated'
             className={cn(
               "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
               isActiveCategoryHidden &&
