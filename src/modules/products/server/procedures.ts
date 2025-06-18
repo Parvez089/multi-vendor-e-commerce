58c61f31f604b011f1639b61e546e259610a172e
parvez <mhparvez089@gmail.com>
2025-06-18 20:26:01 +0600

    12: filtering products

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -8,61 +8,71 @@ import { Category } from "@/payload-types";
 
 
 export const productsRouter = createTRPCRouter({
-  getMany: baseProcedure.
-  input(
-    z.object({
-      category: z.string().nullable().optional(),
-  })
-)
-  .query(async ({ctx, input}) => {
-    const where: Where = {};
+  getMany: baseProcedure
+    .input(
+      z.object({
+        category: z.string().nullable().optional(),
+        minPrice: z.string().nullable().optional(),
+        maxPrice: z.string().nullable().optional(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const where: Where = {};
 
-    if(input.category){
-      const categoriesData = await ctx.db.find({
-        collection: "categories",
-        limit: 1,
-        depth: 1, // Populate subcategories, subcategories.[0] will be a type of "Category"
-        pagination: false,
-        where: {
-          slug: {
-            equals: input.category,
-          },
-        },
-      });
-
-      console.log(JSON.stringify(categoriesData, null, 2))
-       
-       const formattedData = categoriesData.docs.map((doc) => ({
-              ...doc,
-              subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
-                ...(doc as Category),
-                subcategories: undefined,
-              })),
-            }));
-
-      const subcategoriesSlugs = [];    
-      const parentCategory = formattedData[0];
-
-      if (parentCategory) {
-
-        subcategoriesSlugs.push(
-          ...parentCategory.subcategories.map((subcategory) => subcategory.slug)
-        
-        )
-        where["category.slug"] = {
-          in: [parentCategory.slug, ...subcategoriesSlugs],
+      if (input.minPrice) {
+        where.price = {
+          greater_than_equal: input.minPrice,
         };
       }
-    }
-  
-    const data = await ctx.db.find({
-      collection: "products",
-      depth: 1, // Populate "category" & "image"
-      where
-    });
 
+      if (input.maxPrice) {
+        where.price = {
+          less_than_equal: input.maxPrice,
+        };
+      }
 
- 
-    return data;
-  }),
+      if (input.category) {
+        const categoriesData = await ctx.db.find({
+          collection: "categories",
+          limit: 1,
+          depth: 1, // Populate subcategories, subcategories.[0] will be a type of "Category"
+          pagination: false,
+          where: {
+            slug: {
+              equals: input.category,
+            },
+          },
+        });
+
+        const formattedData = categoriesData.docs.map((doc) => ({
+          ...doc,
+          subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+            ...(doc as Category),
+            subcategories: undefined,
+          })),
+        }));
+
+        const subcategoriesSlugs = [];
+        const parentCategory = formattedData[0];
+
+        if (parentCategory) {
+          subcategoriesSlugs.push(
+            ...parentCategory.subcategories.map(
+              (subcategory) => subcategory.slug
+            )
+          );
+          where["category.slug"] = {
+            in: [parentCategory.slug, ...subcategoriesSlugs],
+          };
+        }
+      }
+
+      const data = await ctx.db.find({
+        collection: "products",
+        depth: 1, // Populate "category" & "image"
+        where,
+      });
+
+      return data;
+    }),
 });
