46793197305d62c8ed4b81678e574d0bb1d6c4a6
parvez <mhparvez089@gmail.com>
2025-06-23 11:10:30 +0600

    17: product page

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -15,6 +15,7 @@ import { Products } from "./collections/Products.ts";
 import { Categories } from './collections/Categories.ts'
 import { Tags } from "./collections/Tags.ts";
 import { Tenants } from "./collections/Tenants.ts";
+import { Config } from "./payload-types.ts";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
@@ -38,7 +39,7 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
-    multiTenantPlugin({
+    multiTenantPlugin<Config>({
       collections: {
         products: {},
       },
